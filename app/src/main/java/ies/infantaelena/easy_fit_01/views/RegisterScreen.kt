8cd1eabb616f0b6525fa8a731e0e7a6912cc1238
Jose <josepepo26@gmail.com>
2023-04-12 22:38:05 +0200

    Fin if boton registrar

--- app/src/main/java/ies/infantaelena/easy_fit_01/views/RegisterScreen.kt
+++ app/src/main/java/ies/infantaelena/easy_fit_01/views/RegisterScreen.kt
@@ -252,10 +252,19 @@ fun makeRegister(
     context: Context,
     nav: Any
 ) {
-    fun showAlert(){
+    fun showAlertFail(){
         val builder = AlertDialog.Builder(context)
         builder.setTitle("Error")
         builder.setMessage("Se ha producido un error en el registro")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+        //TODO: hacer el string de los alerts
+    }
+    fun showAlertCorrect(){
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("Exito")
+        builder.setMessage("Se ha registrado con exito")
         builder.setPositiveButton("Aceptar", null)
         val dialog: AlertDialog = builder.create()
         dialog.show()
@@ -272,14 +281,17 @@ fun makeRegister(
         Toast.makeText(context, "Fomato email incorrecto", Toast.LENGTH_SHORT).show()
     } else if (!regex.containsMatchIn(user)) {
         Toast.makeText(context, "Fomato user incorrecto", Toast.LENGTH_SHORT).show()
-    } else if (!password.equals(reppassword)) {
+    } else if (password.length<6){
+        Toast.makeText(context, "Las contraseñas debe tener 6 caracteres", Toast.LENGTH_SHORT).show()
+    }
+    else if (!password.equals(reppassword)) {
         Toast.makeText(context, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
     } else {
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener {
             if (it.isSuccessful){
-                Toast.makeText(context, "Registro Correcto", Toast.LENGTH_SHORT).show()
+               showAlertCorrect()
             }else{
-                showAlert()
+                showAlertFail()
             }
         }
     }
