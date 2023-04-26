39d152587b5f1ac7b8beb3f149ab3110f9974bad
Sorihell <thecustom0.9@gmail.com>
2023-04-26 22:28:26 +0200

    Añadido estilo en la pagina de correr

--- app/src/main/java/ies/infantaelena/easy_fit_01/views/RunActivityScreen.kt
+++ app/src/main/java/ies/infantaelena/easy_fit_01/views/RunActivityScreen.kt
@@ -1,26 +1,35 @@
 package ies.infantaelena.easy_fit_01.views
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Start
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.Circle
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapUiSettings
@@ -37,9 +46,8 @@ fun RunActivityScreen(navController: NavController) {
             .fillMaxSize()
             .background(color = MaterialTheme.colors.background),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top,
+        verticalArrangement = Arrangement.SpaceAround,
     ) {
-        Spacer(modifier = Modifier.padding(5.dp))
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceEvenly,
@@ -52,24 +60,32 @@ fun RunActivityScreen(navController: NavController) {
             )
             Text(text = "Correr", fontSize = 30.sp)
         }
-        Column {
-            val singapore = LatLng(1.35, 103.87)
-            val cameraPositionState = rememberCameraPositionState {
-                position = CameraPosition.fromLatLngZoom(singapore, 10f)
-            }
-            GoogleMap(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(10.dp),
-                cameraPositionState = cameraPositionState,
-                uiSettings = MapUiSettings(zoomControlsEnabled = false)
-            ) {
-                Marker(
-                    state = MarkerState(position = singapore),
-                    title = "Singapore",
-                    snippet = "Marker in Singapore"
-                )
-            }
+        val singapore = LatLng(1.35, 103.87)
+        val cameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(singapore, 10f)
+        }
+        GoogleMap(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(400.dp)
+                .padding(10.dp)
+                .border(width = 5.dp, color = Color.DarkGray, shape = MaterialTheme.shapes.medium),
+            cameraPositionState = cameraPositionState,
+            uiSettings = MapUiSettings(zoomControlsEnabled = false),
+        ) {
+            Marker(
+                state = MarkerState(position = singapore),
+                title = "Singapore",
+                snippet = "Marker in Singapore"
+            )
+        }
+        Text(text = "Tiempo: 1234")
+        Text(text = "Pasos: 1234")
+        Button(
+            modifier = Modifier.size(100.dp),
+            shape = RoundedCornerShape(100),
+            onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Default.Start, contentDescription = "Start Activity")
         }
     }
 }
