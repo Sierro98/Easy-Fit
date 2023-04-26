102b49267182c4ae360e3771bcbb75534b172378
Sorihell <thecustom0.9@gmail.com>
2023-04-26 21:39:23 +0200

    progreso en los mapas, librerias actualizadas

--- app/src/main/java/ies/infantaelena/easy_fit_01/ui/theme/Theme.kt
+++ app/src/main/java/ies/infantaelena/easy_fit_01/ui/theme/Theme.kt
@@ -8,14 +8,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200,
-    onBackground = dark_back,
-    surface = dark_back,
-    onSecondary = teal_green,
-
-    )
+    primary = primary_light,
+    primaryVariant = primaryVariant_ligt,
+    secondary = secondary_light,
+    secondaryVariant = secondaryVariant_light,
+    background = back_light,
+    surface = surface_light,
+    error = error_light,
+    onSecondary = Color.Black,
+    onSurface = Color.Black,
+    onError = Color.Black,
+    onPrimary = Color.Black,
+    onBackground = Color.Black,
+)
 
 private val LightColorPalette = lightColors(
     primary = primary_light,
@@ -35,8 +40,7 @@ private val LightColorPalette = lightColors(
 @Composable
 fun Easy_fit_01Theme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
-        //DarkColorPalette
-        LightColorPalette
+        DarkColorPalette
     } else {
         LightColorPalette
     }
