0e1867ad4740758db06db41f533d001cfc3cbd04
Sorihell <thecustom0.9@gmail.com>
2023-05-06 17:49:19 +0200

    Resuelto bug de inicio xiaomi y añadidas dependencias y ocultada API Key

--- app/src/main/java/ies/infantaelena/easy_fit_01/MainActivity.kt
+++ app/src/main/java/ies/infantaelena/easy_fit_01/MainActivity.kt
@@ -1,33 +1,27 @@
 package ies.infantaelena.easy_fit_01
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.pm.ActivityInfo
 import android.os.Bundle
-import android.widget.Toast
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
-import androidx.biometric.BiometricManager
-import androidx.biometric.BiometricPrompt
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.core.content.ContextCompat
-import androidx.lifecycle.ViewModelProvider
+import androidx.compose.ui.tooling.preview.Preview
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 import ies.infantaelena.easy_fit_01.navigation.AppNavigation
 import ies.infantaelena.easy_fit_01.ui.theme.Easy_fit_01Theme
-import ies.infantaelena.easy_fit_01.viewmodel.LoginViewModel
 
-class MainActivity : AppCompatActivity() {
-
-    // Lo que se ejecuta nada mas crear la AppCompatActivityactividad
+class MainActivity : ComponentActivity() {
+    // Lo que se ejecuta nada mas crear la actividad
     @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         // Funcion para forzar portrait mode
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         super.onCreate(savedInstanceState)
+        handleNavigationBugOnXiaomiDevices()
         setContent {
             Easy_fit_01Theme {
                 val systemUiController = rememberSystemUiController()
@@ -36,11 +30,17 @@ class MainActivity : AppCompatActivity() {
                     darkIcons = false
                 )
                 // Llamada al componente que se encarga de gestionar la navegacion
-                AppNavigation(this)
+                    AppNavigation()
             }
-
         }
     }
 
+    /**
+     * Metodo para solucionar un bug de inicio en los dispositivos de Xiaomi
+     */
+    private fun handleNavigationBugOnXiaomiDevices() {
+        window.decorView.post {
+            window.setBackgroundDrawableResource(android.R.color.background_light)
+        }
+    }
 }
-
